7248f49f8da2d1964e0cffb920aefc7db6c65644
Brandon Lin <blin00@gmail.com>
2018-05-01 07:57:38 -0700

    WTF CFLAG ORDER MATTERS

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-CFLAGS="-g -O3 -march=native -std=c++14 -ffast-math -Wall -Wfatal-errors -Wno-unknown-pragmas $(pkg-config --libs opencv)"
-g++ $CFLAGS -o test test.cpp
-g++ $CFLAGS -fopenmp -o test-omp test.cpp
+CFLAGS="-g -O3 -march=native -std=c++14 -ffast-math -Wall -Wfatal-errors -Wno-unknown-pragmas $(pkg-config --cflags --libs opencv)"
+g++ -o test test.cpp $CFLAGS
+g++ -o test-omp test.cpp -fopenmp $CFLAGS
